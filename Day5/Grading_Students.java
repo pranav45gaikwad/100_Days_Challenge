ec41d61645e48a9b6bcf8529e5a008e5f16e0c11
Pranav Gaikwad <pranavgaikwad480@gmail.com>
2026-01-14 07:01:56 +0530

    Adding First Program of Day 5

---
+++ Day5/Grading_Students.java
@@ -0,0 +1,78 @@
+package Day5;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+    // Write your code here
+    List<Integer> list = new ArrayList<>();
+    
+    for(int i=0;i<grades.size();i++)
+    {
+        int temp=grades.get(i)/5;
+        temp=(temp+1)*5;
+        if(grades.get(i)<38)
+        {
+            list.add(grades.get(i));
+        }
+        else if(temp-grades.get(i)<3)
+        {
+            list.add(temp);
+        }
+        else
+        {
+            list.add(grades.get(i));
+        }
+    }
+    
+    return list;
+
+    }
+
+}
+
+public class Grading_Students {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = new ArrayList<>();
+
+        for (int i = 0; i < gradesCount; i++) {
+            int gradesItem = Integer.parseInt(bufferedReader.readLine().trim());
+            grades.add(gradesItem);
+        }
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
