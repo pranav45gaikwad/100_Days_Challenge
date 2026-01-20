34470cd99ab5ff19fa5ad8fc90dcaafd13760dc1
Pranav Gaikwad <pranavgaikwad480@gmail.com>
2026-01-20 23:06:35 +0530

    Adding Program 1 of Day 11

---
+++ Day11/Area_of_parallelogram.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Area_of_Parallelogram {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int B,H;
+        B = sc.nextInt();
+        H = sc.nextInt();
+        
+        if(B<=0 || H<=0)
+        {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        else
+        {
+           System.out.println(B*H); 
+        }
+    }
+}
+
